2873fd591936b1c42126d9643a4be2396390fed2
zaidan nabil <zaidannabil@gmail.com>
2026-04-13 12:27:00 +0700

    sipesel awal banget

--- pemob_flutter/test/widget_test.dart
+++ pemob_flutter/test/widget_test.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pemob_flutter/main.dart';
+import 'package:sipesel/main.dart';
 
 void main() {
   testWidgets('Smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const SipeselApp());
+    await tester.pumpWidget(const SipeselApp ());
   });
 }
